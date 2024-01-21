803934ed847eb3afc65ae6e375bcbe71488f77eb
Aritra Sarkar <149461282+Aritrao@users.noreply.github.com>
2024-01-21 07:10:57 +0000

    dicussion on variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "sarkararitra687@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId= 2 // not allowed
+
+accountEmail = "srakar@ar.com"
+accountPassword = "212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var because of issue in block and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
