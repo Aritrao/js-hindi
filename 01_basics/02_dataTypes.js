dc2564a1648e7eb7a70c5d2d74fd3013093b4a59
Aritra Sarkar <149461282+Aritrao@users.noreply.github.com>
2024-01-21 07:52:06 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+//alert( 3 + 3) // we are using nodejs, not browser  
+
+console.log(3 + 3)
+console.log("Aritra") //code readability should be high
+
+let name = "Aritra"
+let age = 19
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53 is the range
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => when you declare variables but not assigned the values
+// symbol => unique
+// object
+
+console.log(typeof "Aritra");
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
